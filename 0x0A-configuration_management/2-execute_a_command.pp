9a772e0886de8961d3ba8376be993007d2efc0ab
daffyDev <daffydanni@gmail.com>
2023-04-22 00:58:25 +0100

    done

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# kill process killmenow
+# kills a process named killmenow using exec Puppet resource and pkill
 
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
-}
+exec { 'pkill killmenow':
+  command  =>  '/usr/bin/pkill -f killmenow'
+  }
